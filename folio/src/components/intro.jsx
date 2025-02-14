501644e5823aa54a39c4a5b71026ee39dfc93901
Atharv Remeshan <atharv.rem@gmail.com>
2025-02-14 21:42:26 +0530

    socials

--- folio/src/components/intro.jsx
+++ folio/src/components/intro.jsx
@@ -1,14 +1,17 @@
-import globe from "../assets/globe.svg";
-
 function Intro(){
     return(
         <>
-        <div className= "font-[Aleo] font-medium text-black flex flex-col items-start pb-0 sm:pb-2 md:pb-2 lg:pb-2 xl:pb-2 2xl:pb-2 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-14 2xl:text-8xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl mr-[50px] text-wrap 2xl:leading-[70px] xl:leading-[70px] lg:leading-[70px] md:leading-[70px] sm:leading-[55px]">
+        <div className="flex items-center justify-center pb-0 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow mr-4" />
+            <div className="font-[Fredoka] text-[#9e9e9e] text-[15px] sm:text-xl">intro</div>
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
+      </div>
+        <div className= "font-[Aleo] font-medium text-black flex flex-col items-start pt-2 pb-0 sm:pb-2 md:pb-2 lg:pb-2 xl:pb-2 2xl:pb-2 p-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 2xl:text-8xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl mr-[50px] text-wrap 2xl:leading-[70px] xl:leading-[70px] lg:leading-[70px] md:leading-[70px] sm:leading-[55px]">
             <p>
                 i <span className="font-regular text-[#0548ff] hover:scale-105 inline-flex transition-all duration-300 ease-in-out">code</span> and <span className="font-regular text-[#da15e1] hover:scale-105 inline-flex transition-all duration-300 ease-in-out">design</span> stuff on the internet.
             </p>
         </div>
-        <div className="font-[Fredoka] font-regular text-[#787878] flex flex-col items-start  pt-1 sm:pt-0 md:pt-0 lg:pt-0 xl:pt-0 2xl:pt-0 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-14 2xl:text-2xl lg:text-2xl md:text-2xl sm:text-xl text-xl mr-[50px] text-wrap leading-[24px] sm:leading-[24px] md:leading-[28px] lg:leading-[28px] xl:leading-[28px] 2xl:leading-[28px]">
+        <div className="font-[Fredoka] font-regular text-[#787878] flex flex-col items-start pb-2 pt-1 sm:pt-0 md:pt-0 lg:pt-0 xl:pt-0 2xl:pt-0 p-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 2xl:text-2xl lg:text-2xl md:text-2xl sm:text-xl text-xl mr-[50px] text-wrap leading-[24px] sm:leading-[24px] md:leading-[28px] lg:leading-[28px] xl:leading-[28px] 2xl:leading-[28px]">
             <p>
             Hello there! i'm an 18-year-old kid passionate about coding and designing, and the art of playing the keyboard. chess is my other love—strategic thinking is a part of everything I do. i'm also nurturing a dream of becoming an entrepreneur, and i believe that my diverse interests and skills are paving the way for an exciting entrepreneurial journey. 
             </p>
--- folio/src/components/social.jsx
+++ folio/src/components/social.jsx
@@ -1,12 +1,34 @@
-function Social(){
-    return(
-        <>
-        <div>
-            
-        </div>
-        </>
-    )
+import twitter from "../assets/twitter icon.png";
+import linkedin from "../assets/linkedin icon.png";
+import github from "../assets/github icon.png";
+import instagram from "../assets/instagram icon.png";
+import medium from "../assets/medium icon.png";
 
+function Social() {
+  return (
+    <>
+      <div className="flex items-center justify-center pb-0 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow mr-4" />
+            <div className="font-[Fredoka] text-[#9e9e9e] text-[15px] sm:text-xl">socials</div>
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
+      </div>
+
+      <div className="grid grid-cols-4 gap-1 sm:gap-1 md:gap-2 lg:gap-2 xl:gap-2 2xl:gap-2 mx-6 sm:mx-40 md:mx-50 lg:mx-60 xl:mx-70 2xl:mx-80 mt-1 mb-10 p-2 justify-center justify-items-center">
+        <a href="https://x.com/atharv_rem" target="_blank" rel="noreferrer" className="p-3 rounded-[10px] border-[1px] bg-white border-[#dfdfdf] hover:scale-105">
+          <img src={twitter} alt="X logo" className="aspect-square size-6 object-center" />
+        </a>
+        <a href="https://www.instagram.com/atharv_remeshan" target="_blank" rel="noreferrer" className="p-3 rounded-[10px] border-[1px] bg-white border-[#dfdfdf] hover:scale-105">
+          <img src={instagram} alt="Instagram logo" className="aspect-square size-6 object-center" />
+        </a>
+        <a href="https://github.com/atharv-rem" target="_blank" rel="noreferrer" className="p-3 rounded-[10px] border-[1px] bg-white border-[#dfdfdf] hover:scale-105">
+          <img src={github} alt="GitHub logo" className="aspect-square size-6 object-center" />
+        </a>
+        <a href="https://www.linkedin.com/in/atharv-rem" target="_blank" rel="noreferrer" className="p-3 rounded-[10px] border-[1px] bg-white border-[#dfdfdf] hover:scale-105">
+          <img src={linkedin} alt="LinkedIn logo" className="aspect-square size-6 object-center" />
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Social;
