321fd172d179f384b5f0944969f1dc908f640727
Atharv Remeshan <atharv.rem@gmail.com>
2025-02-23 08:59:28 +0530

    final changes

--- folio/src/components/atharv.jsx
+++ folio/src/components/atharv.jsx
@@ -3,7 +3,7 @@ import favicon from '../assets/favicon.png';
 function Atharv() {
     return (
       <section id="home">
-      <div className="font-[Suwannaphum] text-black w-full h-screen flex flex-col justify-center items-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-14 2xl:text-8xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl drop-shadow-md">
+      <div className="font-[Suwannaphum] text-black w-full h-screen flex flex-col justify-center items-center p-2 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-14 2xl:text-8xl lg:text-8xl md:text-7xl sm:text-6xl text-6xl drop-shadow-md">
         <img src={favicon} alt="favicon" className="w-50 h-50 block sm:hidden mb-10 rounded-[30px] shadow-2xl border-1 border-[#eeeeee]" />
         <p className="hover:scale-110 transition-all duration-300 ease-in-out">ATHARV</p>
         <p className="hover:scale-110 transition-all duration-300 ease-in-out">REMESHAN</p>
