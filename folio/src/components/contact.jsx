32ce57e36958b7dcd7160df88b20c4424e9bec76
Atharv Remeshan <atharv.rem@gmail.com>
2025-02-23 08:29:15 +0530

    bruh

--- folio/src/components/contact.jsx
+++ folio/src/components/contact.jsx
@@ -2,7 +2,7 @@ function Contactme(){
     return (
         <section id="contact" className="w-full min-h-screen flex flex-col">
         {/* Title Section */}
-         <div className="flex items-center justify-center pt-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+        <div className="flex items-center justify-center pt-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
             <div className="h-[1px] bg-[#e9e9e9] flex-grow mr-4" />
             <div className="font-[Fredoka] text-[#9e9e9e] text-[15px] sm:text-xl">contact me</div>
             <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
@@ -17,6 +17,13 @@ function Contactme(){
                 allowFullScreen 
                 title="Contact Form"/>
         </div>
+
+        <div className="flex items-center justify-center pt-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow mr-4" />
+            <div className="font-[Fredoka] text-[#9e9e9e] text-[15px] sm:text-xl">contact me</div>
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
+        </div>
+
         </section>
 
     );
