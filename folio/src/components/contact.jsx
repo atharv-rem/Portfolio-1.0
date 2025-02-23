a485514d275970ec6f510cbfed5a55ccdf870ab5
Atharv Remeshan <atharv.rem@gmail.com>
2025-02-23 06:51:52 +0530

    contact message

--- folio/src/components/contact.jsx
+++ folio/src/components/contact.jsx
@@ -1,12 +1,24 @@
 function Contactme(){
     return (
-        <section id="contact">
-            <div className="flex items-center justify-center pt-10 pb-0 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
-                <div className="h-[1px] bg-[#e9e9e9] flex-grow mr-4" />
-                <div className="font-[Fredoka] text-[#9e9e9e] text-[15px] sm:text-xl">contact me</div>
-                <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
-            </div>
+        <section id="contact" className="w-full min-h-screen flex flex-col">
+        {/* Title Section */}
+         <div className="flex items-center justify-center pt-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow mr-4" />
+            <div className="font-[Fredoka] text-[#9e9e9e] text-[15px] sm:text-xl">contact me</div>
+            <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
+        </div>
+
+        {/* Iframe Container */}
+        <div className="flex-grow flex items-center justify-center px-10 pb-10 mt-5">
+            <iframe 
+                 className="w-full h-[calc(100vh-100px)] sm:h-[calc(100vh-100px)] md:h-[calc(100vh-10px)]  rounded-[30px] shadow-md" 
+                 src="https://formshare.ai/s/smmAnj6XPV" 
+                referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen 
+                title="Contact Form"/>
+        </div>
         </section>
+
     );
 }
 
--- folio/src/components/extras.jsx
+++ folio/src/components/extras.jsx
@@ -15,11 +15,11 @@ function Extras() {
             <div className="h-[1px] bg-[#e9e9e9] flex-grow ml-4" />
         </div>
 
-        <div>
-          <p className="font-[Aleo] font-medium text-black flex flex-col items-start pt-4 pb-0 sm:pb-2 md:pb-2 lg:pb-2 xl:pb-2 2xl:pb-2 p-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 2xl:text-8xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl mr-[80px]"
-          style={{textShadow:"0px 119px 33px rgba(168, 168, 168, 0.00), 0px 76px 30px rgba(168, 168, 168, 0.01), 0px 43px 26px rgba(168, 168, 168, 0.05), 0px 19px 19px rgba(168, 168, 168, 0.09), 0px 5px 10px rgba(168, 168, 168, 0.10)",}}>
-            no more <span className="text-[#DA15E1] inline-flex">boring</span> stock photos
-          </p>
+        <div className= "font-[Aleo] font-medium text-black flex flex-col items-start pt-2 pb-0 sm:pb-2 md:pb-2 lg:pb-2 xl:pb-2 2xl:pb-2 p-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 2xl:text-8xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl mr-[100px] text-wrap 2xl:leading-[85px] xl:leading-[85px] lg:leading-[85px] md:leading-[70px] sm:leading-[55px]"
+        style={{textShadow: '0px 114px 32px rgba(179, 179, 179, 0.00), 0px 73px 29px rgba(179, 179, 179, 0.01), 0px 41px 25px rgba(179, 179, 179, 0.05), 0px 18px 18px rgba(179, 179, 179, 0.09), 0px 5px 10px rgba(179, 179, 179, 0.10)'}}>
+            <p>
+                <span className="font-regular text-black hover:scale-105 inline-flex transition-all duration-300 ease-in-out">no</span> more <span className="font-regular text-black hover:scale-105 inline-flex transition-all duration-300 ease-in-out"> boring </span> stock photos.
+            </p>
         </div>
 
         <div className="font-[Fredoka] font-regular text-[#787878] flex flex-col items-start pb-2 pt-1 sm:pt-0 md:pt-0 lg:pt-0 xl:pt-0 2xl:pt-0 p-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 2xl:text-2xl lg:text-2xl md:text-2xl sm:text-xl text-xl mr-[50px] text-wrap leading-[24px] sm:leading-[24px] md:leading-[28px] lg:leading-[28px] xl:leading-[28px] 2xl:leading-[28px]">
@@ -29,16 +29,16 @@ function Extras() {
         </div>
 
         <div className="flex pt-4 pb-4 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 justify-start items-center w-full">
-          <div className="font-[Doto] font-extrabold text-black text-2xl rounded-[15px] p-4 justify-center items-center shadow-sm sm:shadow-sm hover:shadow-md">
+          <div className="font-[Sen] border-1 border-[#f2f2f2] font-bold text-[#3f3f3f] text-3xl rounded-[15px] p-4 justify-center items-center shadow-sm sm:shadow-sm hover:shadow-md">
             9.2k+ views
           </div>
-          <div className="font-[Doto] font-extrabold text-black text-2xl rounded-[15px] p-4 justify-center items-center shadow-sm sm:shadow-sm hover:shadow-md mx-4">
+          <div className="font-[Sen] border-1 border-[#f2f2f2] font-bold text-[#3f3f3f] text-3xl rounded-[15px] p-4 justify-center items-center shadow-sm sm:shadow-sm hover:shadow-md mx-4">
             3.2k+ downloads
           </div>
         </div>
-        <div className="flex pt-1 pb-4 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 justify-start items-center w-full">
+        <div className="flex pt-1 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 justify-start items-center w-full">
           <a href="https://www.lummi.ai/creator/atharv-rem" target="_blank" rel="noreferrer">
-          <div className="font-[Doto] font-extrabold text-black text-2xl rounded-[15px] p-4 justify-center items-center shadow-sm sm:shadow-sm hover:shadow-md">
+          <div className="hover:scale-110 font-[Sen] font-bold text-[#3f3f3f] bg-white border-1 border-[#f2f2f2] text-2xl rounded-[15px] p-4 justify-center items-center shadow-sm sm:shadow-sm hover:shadow-md">
             Visit account
           </div>
           </a>
